67be6714f310b609359e334b24ea2b0e09c0ef31
abdullahgzbyk <abdullahgzbyk@gmail.com>
2024-05-21 22:40:37 +0300

    diziler

---
+++ kutuphane/dizi.js
@@ -0,0 +1,49 @@
+
+
+const meyveler = [
+    {
+        "isim": "elma",
+        "fiyat": "25"
+    },
+    {
+        "isim": "armut",
+        "fiyat": "30"
+    },
+    {
+        "isim": "portakal",
+        "fiyat": "20"
+    },
+    {
+        "isim": "muz",
+        "fiyat": "35"
+    }
+    
+]
+console.log(meyveler);
+
+for (let index=0;index < meyveler.length;index++){
+    console.log("index degeri: ",index,meyveler[index])
+}
+
+
+
+const currencies = [
+    { code: "USD", name: "US Dollar", symbol: "$" },
+    { code: "EUR", name: "Euro", symbol: "€" },
+    { code: "JPY", name: "Japanese Yen", symbol: "¥" },
+    { code: "GBP", name: "British Pound", symbol: "£" },
+    { code: "AUD", name: "Australian Dollar", symbol: "A$" },
+    { code: "CHF", name: "Swiss Franc", symbol: "CHF" },
+    { code: "CNY", name: "Chinese Yuan Renminbi", symbol: "¥" },
+    { code: "CAD", name: "Canadian Dollar", symbol: "$" },
+    { code: "NZD", name: "New Zealand Dollar", symbol: "$" },
+    { code: "SEK", name: "Swedish Krona", symbol: "kr" },
+  ];
+  
+    console.log (currencies)
+    const tablo = document.createElement('tablo');
+    
+  
+   
+    
+ 
